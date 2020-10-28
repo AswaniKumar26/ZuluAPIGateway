f1a668c037f058ccb194b8572632a16044c9113e
AswaniKumar26 <69358032+AswaniKumar26@users.noreply.github.com>
2020-10-28 18:53:09 +0530

    Zuulu API gateway

---
+++ src/test/java/com/learning/microservice/gateway/zulu/ZuluApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.microservice.gateway.zulu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuluApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
